e187b10c603af188c1ddce43c9ae0e7d21aec11b
Michele Riva <master@micheleriva.it>
2018-12-05 12:38:25 +0100

    added metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,24 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Michele Riva - Software Engineer',
+    title: 'Michele Riva | Software Engineer',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Michele Riva - Software Engineer, Fullstack Developer, OpenSource Contributor' }
+      { name: 'viewport',                         content: 'width=device-width, initial-scale=1' },
+      { hid:  'description', name: 'description', content: 'Michele Riva | Software Engineer, Fullstack Developer, OpenSource Contributor' },
+      { name: 'keywords',                         content: 'software engineer, node, nodejs, go, golang, javascript, es6, haskell, reasonml, development' },
+      { name: 'theme-color',                      content: '#050817' },
+
+      { property: 'og:type',        content: 'website' },
+      { property: 'og:title',       content: 'Michele Riva | Software Engineer' },
+      { property: 'og:description', content: 'Software Engineer, Fullstack Developer and OpenSource Contributor' },
+      { property: 'og:image',       content: '/imgs/micheleriva-ogimg.jpg' },
+
+      { property: 'twitter:site',        content: '@MicheleRiva95' },
+      { property: 'twitter:card',        content: 'summary_large_image' },
+      { property: 'twitter:url',         content: 'https://www.micheleriva.it' },
+      { property: 'twitter:image',       content: '/imgs/micheleriva-ogimg.jpg' },
+      { property: 'twitter:description', content: 'Software Engineer, Fullstack Developer and OpenSource Contributor' }
     ],
     link: [
       { rel: 'manifest',                         href: '/manifest.json' },
