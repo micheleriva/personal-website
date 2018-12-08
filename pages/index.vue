932f3e03f06d52dedff24e6d4aebb5db249e977f
Michele Riva <master@micheleriva.it>
2018-12-08 19:43:19 +0100

    refactoring home

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
   div
     .cont
-      h1 Michele Riva
+      h1.fadeInDown.animated Michele Riva
       text-rotator(
         :words="skills"
         :speed="4000"
         :animations="['fadeInUp', 'fadeOutUp']"
       )
-      .scroll-down(v-scroll-to="'#software-engineer'")
-        .pulse
-        img(src="/imgs/svgs/objects/scroll-down.svg")
+
+      .sections.fadeInUp.animated
+        nuxt-link.section(
+          v-for="section in sections"
+          :class="section.class"
+          :to="section.url"
+        ) {{ section.name }}
 
 </template>
 
@@ -29,7 +33,29 @@ export default {
 
   data() {
     return {
-      skills: Presentation
+      skills: Presentation,
+      sections: [
+        {
+          url: '/software-engineer',
+          name: 'Software Engineer',
+          class: 'se'
+        },
+        {
+          url: '/fullstack-developer',
+          name: 'Fullstack Developer',
+          class: 'fd'
+        },
+        {
+          url: '/opensource-contributor',
+          name: 'OpenSource Contributor',
+          class: 'oc'
+        },
+        {
+          url: '/writer',
+          name: 'Writer',
+          class: 'wr'
+        }
+      ]
     }
   },
 
@@ -47,47 +73,43 @@ export default {
   @import '~/assets/main.scss';
   @import '~/assets/landing.scss';
 
-  $scroll-down-cta-gradient: linear-gradient( 135deg, #EECE13 10%, #B210FF 100%);
-
-  .scroll-down {
+  .sections {
     display: flex;
-    width: 50px;
-    height: 50px;
-    border-radius: 100%;
-    justify-content: center;
-    align-items: center;
+    flex-wrap: wrap;
     margin-top: 4em;
-    cursor: pointer;
-    background-image: $scroll-down-cta-gradient;
-    z-index: 1;
-    
-    .pulse {
-      position: absolute;
-      border-radius: 100%;
-      height: 60px;
-      width: 60px;
-      background-image: $scroll-down-cta-gradient;
-      animation: pulse infinite 2s linear;
-    }
-
-    img {
-      max-width: 50px;
-      margin-top: 5px;
-    }
   }
 
-  @keyframes pulse {
-	0% {
-		transform: scale(1, 1);
-		opacity: 0;
+  .section {
+    padding: 2em;
+    min-width: 250px;
+    text-align: center;
+    margin-left: 5px;
+    margin-right: 5px;
+    border-radius: 15px;
+    text-decoration: none;
+    color: $white;
+    font-weight: 700;
+    transition: ease 0.5s;
+
+    &:hover {
+      transform: translateY(-1em);
+    }
+
+    &.se {
+      background-image: linear-gradient( 135deg, #EECE13 10%, #B210FF 100%);
+    }
+
+    &.fd {
+      background-image: linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%);
+    }
+
+    &.oc {
+      background-image: linear-gradient( 135deg, #70F570 10%, #49C628 100%);
+    }
+
+    &.wr {
+      background-image: linear-gradient( 135deg, #F97794 10%, #623AA2 100%);
+    }
   }
-  15% {
-    opacity: 0.5;
-  }
-	50%, 100% {
-		transform: scale(1.25, 1.25);
-		opacity: 0;
-	}
-}
-  
+
 </style>
