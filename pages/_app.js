ac0e314e11b99deb8fca6c32d70cccda79715d1b
Michele Riva <master@micheleriva.it>
2020-11-30 15:22:47 +0100

    feat(analytics): added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps }) {
           key="twitter:title"
         />
         <meta name="twitter:image" content="/ogimage.png" key="twitter:image" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-Equiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* GOOGLE ANALYTICS */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-76573733-1" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-UA-76573733-1');
+            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
