5c09d6fd3948f439461a89d39dce16d7e5448e67
Michele Riva <master@micheleriva.it>
2020-12-01 18:08:39 +0100

    feat(nowplaying): added spotify player

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -1,28 +1,21 @@
-// All credits to https://github.com/leerob/leerob.io
-
 import { getNowPlaying } from '../../lib/spotify';
 
 export default async (_, res) => {
   const response = await getNowPlaying();
 
   if (response.status === 204 || response.status > 400) {
-    return res.status(200).json({ isPlaying: false });
+    return res.status(200).json({ track: { isPlaying: false, cover: '/violin-meme.jpg' } });
   }
 
-  const song = await response.json();
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
-  const album = song.item.album.name;
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
+  const { item } = await response.json();
+  const track = {
+    isPlaying: true,
+    artist: item.artists.map((_artist) => _artist.name).join(', '),
+    songUrl: item.external_urls.spotify,
+    title: item.name,
+    cover: item.album.images[0].url,
+    album: item.album.name,
+  };
 
-  return res.status(200).json({
-    album,
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title,
-  });
+  return res.status(200).json({ track });
 };
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Button } from '@chakra-ui/react';
 import { Heading, Divider } from '@chakra-ui/react';
 import Bio from '../components/Bio';
 import Featured from '../components/Featured';
+import NowPlaying from '../components/NowPlaying';
 
 export function getStaticProps() {
   return {
@@ -18,6 +19,11 @@ function Home() {
       <Bio />
       <Divider mt="2rem" mb="2rem" />
       <Featured />
+      <Divider mt="2rem" mb="2rem" />
+      <Heading size="xl" mb="1rem">
+        I'm currently listening
+      </Heading>
+      <NowPlaying />
     </>
   );
 }
