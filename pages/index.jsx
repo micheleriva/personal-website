86d11104163501005b5e4978bce8cf7737c3feb3
Michele Riva <ciao@micheleriva.it>
2022-11-20 11:04:36 +0100

    updated home

--- pages/index.jsx
+++ pages/index.jsx
@@ -170,7 +170,7 @@ export default function Home() {
           <div className="grid gap-5">
             <iframe
               className="w-[100%] md:h-[315px]"
-              src="https://www.youtube.com/embed/MGpwKsdZmG0"
+              src="https://www.youtube.com/embed/42sMkbGLlh4"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -178,7 +178,7 @@ export default function Home() {
             ></iframe>
             <iframe
               className="w-[100%] md:h-[315px]"
-              src="https://www.youtube.com/embed/hRJ7MYwJezM"
+              src="https://www.youtube.com/embed/MGpwKsdZmG0"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
