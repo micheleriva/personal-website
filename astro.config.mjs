53c02e3608e6ab7faa433b47b3cbf17119974194
Michele Riva <michele.riva@oramasearch.com>
2025-07-20 10:21:15 -0700

    improves website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
-  integrations: [tailwind(), react({ experimentalReactChildren: true })],
+  integrations: [tailwind(), react({ experimentalReactChildren: true }), mdx()],
 });
